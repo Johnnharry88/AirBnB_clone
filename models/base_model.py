724aa2bbbe981aba24ed729fa6647fd7bb538474
Johnharry88 <harryjohnwhye@gmail.com>
2024-05-06 15:48:46 +0100

    AirBnB_clone

--- models/base_model.py
+++ models/base_model.py
@@ -29,8 +29,6 @@ class BaseModel():
                 setattr(self, key, value)
             return
 
-        self.name = args
-        self.my_number = args
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
